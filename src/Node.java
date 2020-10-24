0af600fd31bb4c77e071934c019749741dd0c321
Divya Chakkaram <dchakkaram@smu.edu>
2020-10-23 22:35:24 -0500

    second commit, coded more in tree class, train method as well as the node class

--- src/Node.java
+++ src/Node.java
@@ -1,31 +1,49 @@
 import java.util.ArrayList;
 
-public class Node {
-	int tokenSequence;
+public class Node<T> extends Tree<T> {
+
+	Node(int length) {
+		super(length);
+		// TODO Auto-generated constructor stub
+	}
+
+	ArrayList<T> tokenSequence;
 	ArrayList<Node> children;
-	
+
 	boolean addNode(Node node) {
 		boolean found = false;
 
-		if (node.tokenSequence == node.tokenSequence) { // if token sequence of THIS node is the token sequence
+		if (node.tokenSequence == root.tokenSequence) { // if token sequence of THIS node is the token sequence of the
+														// added node
 			found = true;
-		} // else if (amIASuffix(node == tokenSequence.size() == 0){
+		} else if (amIASuffix(node) || (tokenSequence.size() == 0)) {
 
-		// }
+			// 1.try to add the node to all the children nodes.
+			// 2.Did one your child nodes add the node? **keep track of this via the found
+			// variable**
+			// If NOT found and the length of node’s tokenSequence is one less than this
+			// tokenSequenceAdd the node to our children array.
+			// Thus-found=true.children.add(node);
+
+			//what does everything above this say
+			if (found = true) { 
+				children.add(node);
+			}
+
+		}
 		return found;
 
 	}
-	
-	void print(){
-		
+
+	void print() {
+
 	}
 
 	boolean amIASuffix(Node node) {
+		//try to figure this out using notes
 
 		return false;
 
 	}
-
-
 
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
 
-public class Tree extends Node {
+public class Tree<T> {
 	Node root;
 	int L; // maximum token sequence length
 
-	void train() {
+	Tree(int length) {
+		L = length;
+
+	}
+
+	void train(ArrayList<T> input) {
+		for (int i = L; i < L; i++) { // i is the current order number
+
+			for (int j = 0; j < input.size() - (i - 1); j++) { // j is the index into the input
+				Node curSequence = (Node) input.get(j);
+				Node newNode = curSequence;
+				root.addNode(newNode);
+
+			}
+		}
 
 	}
 
